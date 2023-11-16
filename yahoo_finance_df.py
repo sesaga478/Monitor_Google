b62b875f96e8c62c5a0566025026617fd01aff46
Sebastián Salazar <54647017+sesaga478@users.noreply.github.com>
2023-11-16 09:37:51 -0500

    Update yahoo_finance_df.py

--- yahoo_finance_df.py
+++ yahoo_finance_df.py
@@ -1,7 +1,7 @@
 import yfinance as yf
 import pandas as pd
 
-def yahoo_fin_df(sector,ticker, start_date, end_date):
+def yahoo_fin_df(sector,ticker, company, start_date, end_date):
     data = yf.download(ticker, start=start_date, end=end_date)
     df=pd.DataFrame(data['Adj Close'])
     df['sector']=sector
