5301d5386bb60434ecb9b644308305a223121ee1
Sebastián Salazar <54647017+sesaga478@users.noreply.github.com>
2023-11-16 09:33:43 -0500

    Update and rename yahoo_fin_df.py to yahoo_finance_df.py

---
+++ yahoo_finance_df.py
@@ -0,0 +1,9 @@
+import yfinance as yf
+import pandas as pd
+
+def yahoo_fin_df(sector,ticker, start_date, end_date):
+    data = yf.download(ticker, start=start_date, end=end_date)
+    df=pd.DataFrame(data['Adj Close'])
+    df['sector']=sector
+    df['ticker']=ticker
+    return df
